550aeb5beaef22c9291bae092a83db0a0a7d1184
jeaniewwu <95399587+jeaniewwu@users.noreply.github.com>
2022-01-27 13:14:38 -0800

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,10 +5,10 @@ import csv
 csvpath=os.path.join('..','Resources','election_data.csv')
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
-    csv_header = next(csvreader)
-    
    
+
+#read the header row first
+csv_header = next(csvreader)
     votes = []
     county = []
     candidates = []
